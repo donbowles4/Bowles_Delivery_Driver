27db9c8717b225735c82ae9b52fd08ae7aec2a92
Donald Bowles <donniebowles84@gmail.com>
2022-02-10 17:14:29 -0500

    If Statements & Tags

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collision : MonoBehaviour
+public class Delivery : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D other) 
     {
@@ -10,6 +10,13 @@ public class Collision : MonoBehaviour
     }
     void OnTriggerEnter2D(Collider2D other)
      {
-        Debug.Log("I am triggered!!");
+        if (other.tag == "Package")
+        {
+        Debug.Log("Package picked up.");
+        }
+        if (other.tag == "Customer")
+        {
+        Debug.Log("Package Delivered.");
+        }
     }
 }
